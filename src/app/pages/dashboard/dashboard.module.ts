8843c1c538213b5a9dbd2cd06a9efea2c4371300
jose <jose.romero@proveedor.ues21.edu.ar>
2024-07-18 23:50:59 -0300

    cambios varios

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { CoursesModule } from './courses/courses.module';
+import {MatListModule} from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  exports: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    CoursesModule,
+    MatListModule
+  ]
+})
+export class DashboardModule { }
