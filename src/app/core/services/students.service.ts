3e7e250e458fc5402c837d4b32ab9006d919979b
jose <jose.romero@proveedor.ues21.edu.ar>
2024-08-09 09:56:28 -0300

    modificacion nombre objeto students

--- src/app/core/services/students.service.ts
+++ src/app/core/services/students.service.ts
@@ -33,19 +33,19 @@ export class StudentsService {
   constructor(private httpClient: HttpClient) { }
 
   getStudents(): Observable<Student[]> {
-      return this.httpClient.get<Student[]>(environment.apiUrl + '/student')
+      return this.httpClient.get<Student[]>(environment.apiUrl + '/students')
   }
 
   addStudents(student: Student): Observable<Student[]>{
-    return this.httpClient.post<Student[]>(environment.apiUrl + '/student', student)
+    return this.httpClient.post<Student[]>(environment.apiUrl + '/students', student)
   }
 
   editStudentByDni(id: string, update: Student) {
-    return this.httpClient.put(environment.apiUrl + '/student/' + id, update)
+    return this.httpClient.put(environment.apiUrl + '/students/' + id, update)
   }
 
   deleteStudentByDni(id: string): Observable<Student[]> {
-    return this.httpClient.delete<Student[]>(environment.apiUrl + '/student/' + id)
+    return this.httpClient.delete<Student[]>(environment.apiUrl + '/students/' + id)
   }
 
 }
